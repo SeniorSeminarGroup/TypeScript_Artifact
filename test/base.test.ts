d38e39a3b4c586feb2f50a7b227d6858f43388af
Nam Bui <namhaibui693@gmail.com>
2024-10-28 00:33:25 -0400

    Fixed ReferenceError

--- test/base.test.ts
+++
@@ -1,9 +0,0 @@
-
-import { expect } from 'chai';
-import { add } from '../src/calculator';
-
-describe('Calculator', () => {
-    it('should add two numbers correctly', () => {
-        expect(add(2, 3)).to.equal(5);
-    });
-});
